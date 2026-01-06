3b1a2125c28ca0a3b4b51b43ff4c2a92b7ba6726
reme25241-cloud <reme25241@gmail.com>
2026-01-06 16:06:33 +0530

    Update admin.py

--- alerts/admin.py
+++ alerts/admin.py
@@ -1,3 +1,17 @@
+# alerts/admin.py
 from django.contrib import admin
+from .models import IoTAlert
 
-# Register your models here.
+@admin.register(IoTAlert)
+class IoTAlertAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "relay",
+        "temperature",
+        "humidity",
+        "soil_value",
+        "alert_triggered",
+        "created_at",
+    )
+    list_filter = ("alert_triggered", "relay")
+    search_fields = ("relay",)
