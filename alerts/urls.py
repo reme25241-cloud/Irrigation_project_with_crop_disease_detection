b7f7bba5c0948daca393f81ac0737925f9d80ee4
reme25241-cloud <reme25241@gmail.com>
2026-01-06 16:05:56 +0530

    Update urls.py

--- alerts/urls.py
+++ alerts/urls.py
@@ -1,7 +1,8 @@
 # alerts/urls.py
 from django.urls import path
-from .views import iot_alert
+from .views import *
 
 urlpatterns = [
-    path("api/iot-alert/", iot_alert),
+    path("api/iot-alert/", iot_alert, name="iot_alert"),
+    path("alert_dashboard/", alert_dashboard, name="alert_dashboard"),
 ]
