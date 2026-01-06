47a39364339a46d3206951636f7d4ca3af4a2808
reme25241-cloud <reme25241@gmail.com>
2026-01-06 16:06:13 +0530

    Update models.py

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class IoTAlert(models.Model):
+    soil_value = models.IntegerField(null=True, blank=True)
+    temperature = models.FloatField()
+    humidity = models.FloatField(null=True, blank=True)
+    relay = models.CharField(max_length=20)
+    alert_triggered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.relay} | {self.temperature}°C | {self.created_at}"
