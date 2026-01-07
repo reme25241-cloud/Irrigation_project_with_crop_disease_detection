39f8b4c0c59dc147e622646b9b2b8f1e5746d90d
reme25241-cloud <reme25241@gmail.com>
2026-01-07 19:26:42 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -147,17 +147,16 @@ LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = '@gmail.com'  
-# EMAIL_HOST_PASSWORD = '**** **** **** ****'  
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'poojabenade12@gmail.com'  
+EMAIL_HOST_PASSWORD = 'vhec btkm ukyk aokt'  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
